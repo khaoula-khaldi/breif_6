966034e2746d3750131d46e2169329b7987df85f
khaoula khaldi <khaoula2417@gmail.com>
2025-11-17 17:30:13 +0100

    modifier quellque chose dans grid et ajouter des fonctionnaliter pour valider les input qui utilisateur enter

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-
+////validation des input des formulaire **************************
 let btn_envoyer = document.getElementById('btn_envoyer');
 
 btn_envoyer.onclick = (e) => {
@@ -8,25 +8,46 @@ btn_envoyer.onclick = (e) => {
     let telephone = document.getElementById("telephone").value.trim();
     let experiences = document.getElementById("Experiences").value.trim();
 
-    //validation ta3 nome
-    const nomRegex = /^[A-Za-z]+$/;
+
     // wash dook les input khaween
     if (nom === "" || email === "" || telephone === "" || experiences === "") {
         alert("Remplir tous les champs !!");
         return;
     }
+    //validation ta3 nome
+    const nomRegex = /^[a-zA-Z]+$/;
 
-    // validation ta3 nome
     if (!nomRegex.test(nom)) {
-        alert("Nom incorrect ");
+        alert("Nom incorrect !");
         return;
     } 
     // validation ta3 email
-    const emailRegex = /^[A-Za-z1_10]+@+email.com+$/;
-    if(!emailRegex.test(email)) {
-        alert("Email incorrect ");
-        return;
-    } 
+    const emailRegex = /^[a-zA-Z]+[0-9]+@+gmail.com+$/;
 
-   
+    if(!emailRegex.test(email)) {
+        alert("Email incorrect !");
+        return;
+    }
+    
+    
+    // // validation ta3 telephone
+    // const teleohoneRegex = /^\(+212|0)(6|7|5)[0-9]{9}$/;
+
+    // if(!teleohoneRegex.test(telephone)){
+    //     alert("telephone incorrect !");
+    //     return;
+    // }
+
+
+    //validation ta3 experience
+    const experiencesRegex =/^[a-zA-Z]+$/;
+
+    if(!experiencesRegex.test(experiences)){
+         alert("remplire une vrai experience !");
+        return;
+    }
+    //validation de url
 }
+///tableau dans local storeg qui stokes les info les employer
+
+
