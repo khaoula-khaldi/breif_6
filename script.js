60d76388f6fc976c348db22b6cdd1d37f1b49f76
khaoula khaldi <khaoula2417@gmail.com>
2025-11-18 10:31:41 +0100

    la fonction de lka formulaire dynamique et travailler

--- script.js
+++ script.js
@@ -30,71 +30,58 @@ btn_envoyer.onclick = (e) => {
     }
 
 
-    // // validation ta3 telephone
-    // const telephoneRegex = /^\(+212|0)(6|7|5)[0-9]{9}$/;
+    // validation ta3 telephone
+    const telephoneRegex =  /^\+212[0-9]{9}$/;
 
-    // if(!teleohoneRegex.test(telephone)){
-    //     alert("telephone incorrect !");
-    //     return;
-    // }
+    if(!telephoneRegex.test(telephone)){
+        alert("telephone incorrect !");
+        return;
+    }
 
-    // //validation ta3 experience
-    // const experiencesRegex = /^[a-zA-Z]+$/;
+    //validation ta3 experience
+    const experiencesRegex = /^[a-zA-Z]+$/;
 
-    // if (!experiencesRegex.test(experiences)) {
-    //     alert("remplire une vrai experience !");
-    //     return;
-    // }
+    if (!experiencesRegex.test(experiences)) {
+        alert("remplire une vrai experience !");
+        return;
+    }
 
 
     //validation de url
 }
 
-
-///formulaire dynamique ***********************************
-let ExperiencesContainer = getElementById("ExperiencesContainer");
+/// formulaire dynamique ***********************************
+let ExperiencesContainer = document.getElementById("ExperiencesContainer");
 let btnPlus = document.getElementById("btn_plus");
-btnPlus.addEventListener('click',function (e){
-        ExperiencesContainer.innerHTML = "";
-    const experiencePLUS = document.createElement("input");
-    experiencePLUS.className = "border border-solide-balck rounded px-5";
-    experiencePLUS.id = `Experiences-${experiencesPLUS.id}`;
-    ExperiencesContainer.appendChild(experiencesPLUS);
-})
-//ajouter un employe
-let ajouterEmploye = document.getElementById('ajouterEmploye');
-ajouterEmploye.addEventListener('click',(e)=>{
-    formulaire.style.display = 'block';
-});
-//afficher les employee
-let afficherEmployees = document.getElementById("afficherEmployees");
-afficherEmployees.addEventListener('click',(e)=>{
 
-})
-//AJOUTER un employe dans un chambre 
-let btn_ajoute = document.getElementById("btn_ajoute");
-btn_ajoute . addEventListener ('click',(e)=>{
-    let employe = document.createElement("section");
+btnPlus.addEventListener('click', function () {
     
+    const experiencePLUS = document.createElement("input");
+    experiencePLUS.className = "border border-solide-balck rounded px-5 py-2";
+    experiencePLUS.placeholder = "Ajouter autre expérience";
 
-})
+    ExperiencesContainer.appendChild(experiencePLUS);
+});
+
+// //ajouter un employe
+// let ajouterEmploye = document.getElementById('ajouterEmploye');
+// ajouterEmploye.addEventListener('click',(e)=>{
+//     formulaire.style.display = 'block';
+// });
+// //afficher les employee
+// let afficherEmployees = document.getElementById("afficherEmployees");
+// afficherEmployees.addEventListener('click',(e)=>{
+
+// })
+// //AJOUTER un employe dans un chambre 
+// let btn_ajoute = document.getElementById("btn_ajoute");
+// btn_ajoute . addEventListener ('click',(e)=>{
+//     let employe = document.createElement("section");
+
+
+// })
 
 
 
 
 
-
-
-//   document.addEventListener('click', function (e) {
-//             if (e.target.classList.contains('clone-btn')) {
-//                 const row = e.target.closest('.clonable-row');
-//                 const newRow = row.cloneNode(true);
-//                 newRow.querySelector('input').value = '';
-//                 newRow.querySelector('button').textContent = '-';
-//                 newRow.querySelector('button').className = 'remove-btn';
-//                 row.parentNode.appendChild(newRow);
-//             }
-//             else if (e.target.classList.contains('remove-btn')) {
-//                 e.target.closest('.clonable-row').remove();
-//             }
-//         });
